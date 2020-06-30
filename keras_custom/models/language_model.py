ac34b8e7ed9d61ba5ef811c2f598bb12c4e43a72
Xiaoliang (Ken) Luo <ken@love21.pals.ucl.ac.uk>
2020-06-30 13:13:44 +0100

    decoupled lang model into semantic and discrete models

--- keras_custom/models/language_model.py
+++ keras_custom/models/language_model.py
@@ -1,7 +1,7 @@
 import os
-os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"]= '4'
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+# os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+# os.environ["CUDA_VISIBLE_DEVICES"]= '3'
+# os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 import numpy as np
 
@@ -16,54 +16,55 @@ from tensorflow.keras.applications.vgg16 import VGG16
 from tensorflow.keras.layers import Layer, Flatten, Reshape, Dense, \
      Dropout, Input, Multiply, Dot, Concatenate, BatchNormalization
 
-# from keras_custom.layers.attention_model_layers import EmbedSemantics, \
-#      FineTuneOutput, ProduceAttentionWeights, ExpandAttention
-
 """
-The Language Model
+The Language Models
 """
 
-# def lang_model(num_labels=1000, seed=42):
-#      """
-#      inputs:
-#      ------
-#           num_labels: label output layer size
-#      """
-#      vgg = VGG16(weights='imagenet', include_top=True, input_shape=(224, 224, 3))
-#      x = vgg.input
-#      # [1,-2] means skip input layer and stop at last FC.
-#      for layer in vgg.layers[1:-1]:
-#           layer.trainable = False
-#           x = layer(x)
-
-#      # attach the semantic layer (the end of the branch)
-#      semantic_output = Dense(768, activation='sigmoid', name='semantic_output',
-#                kernel_initializer=keras.initializers.glorot_normal(seed=seed))(x)
-
-#      semantic_intermediate = Dense(768, activation='sigmoid', name='semantic_intermediate',
-#                kernel_initializer=keras.initializers.glorot_normal(seed=seed))(x)
-
-#      # attach discrete label layer.
-#      label_output = Dense(num_labels, activation='softmax', name='label_output',
-#                kernel_initializer=keras.initializers.glorot_normal(seed=seed))(semantic_intermediate)
-
-#      model = Model(inputs=vgg.input, outputs=[semantic_output, label_output])
-#      return model
-
-#      #from tensorflow.keras.utils import plot_model
-#      #plot_model(model, to_file='lang_model.png')
-#      #model.summary()
-
-
-# temp: only has semantic output.
-def lang_model(num_labels=1000, seed=42):
+# temp: only has discrete labelling output
+def lang_model_discrete(num_labels=1000, seed=42):
      """
      inputs:
      ------
           num_labels: label output layer size
      """
      vgg = VGG16(weights='imagenet', include_top=True, input_shape=(224, 224, 3))
+     # load fine tuned weights:
+     vgg.load_weights('_trained_weights/VGG16_finetuned_fullmodelWeights.h5')
+     print('loaded in fine tuned VGG16 weights.')
 
+     x = vgg.input
+     # [1,-2] means skip input layer and stop at last FC.
+     for layer in vgg.layers[1:-1]:
+          layer.trainable = False
+          x = layer(x)
+
+     semantic_intermediate = Dense(768, activation='sigmoid', name='semantic_intermediate',
+               kernel_initializer=keras.initializers.glorot_normal(seed=seed))(x)
+
+     # attach discrete label layer.
+     label_output = Dense(num_labels, activation='softmax', name='label_output',
+               kernel_initializer=keras.initializers.glorot_normal(seed=seed))(semantic_intermediate)
+
+     model = Model(inputs=vgg.input, outputs=label_output)
+
+     # need to load in the trained weights for semantic layer from 
+     # semantic only model
+     trained_weights = np.load('_trained_weights/semantic_output_weights=29-06-20.npy',
+                              allow_pickle=True)
+     model.get_layer('semantic_intermediate').set_weights([trained_weights[0], trained_weights[1]])
+
+     model.summary()
+     return model
+
+
+# temp: only has semantic output.
+def lang_model_semantic(num_labels=1000, seed=42):
+     """
+     inputs:
+     ------
+          num_labels: label output layer size
+     """
+     vgg = VGG16(weights='imagenet', include_top=True, input_shape=(224, 224, 3))
      # load fine tuned weights:
      vgg.load_weights('trained_weights/VGG16_finetuned_fullmodelWeights.h5')
      print('loaded in fine tuned VGG16 weights.')
@@ -84,6 +85,5 @@ def lang_model(num_labels=1000, seed=42):
 
 
 if __name__ == '__main__':
-     lang_model()
-
+     lang_model_discrete()
 
