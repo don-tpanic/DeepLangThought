e524c64156ec6ccbab12fd8c00e383e0f63106b8
Xiaoliang (Ken) Luo <ken@love21.pals.ucl.ac.uk>
2020-06-30 18:10:06 +0100

    gu2

---
+++ META_eval.py
@@ -0,0 +1,5 @@
+#from EVAL import eval_trained_model
+#eval_trained_model.execute()
+
+from EVAL import compare_semantic_activations
+compare_semantic_activations.execute()
--- META_train.py
+++ META_train.py
@@ -1,5 +1,5 @@
 """
 META train
 """
-from TRAIN import train_language_model 
-train_language_model.execute()
+from TRAIN import train_language_model
+train_language_model.execute_discrete_labelling()
