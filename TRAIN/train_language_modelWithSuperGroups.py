75e62e35d9a96936bcb59732779635e99d2c3c61
Xiaoliang (Ken) Luo <ken@love21.pals.ucl.ac.uk>
2020-07-16 23:19:06 +0100

    superGroup first implementation seems to be working...

---
+++ TRAIN/train_language_modelWithSuperGroups.py
@@ -0,0 +1,130 @@
+"""
+Temp test script for training language model 
+with similar classes grouped into ONE group 
+when training the discrete term.
+"""
+import os
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"]= '1'
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+import numpy as np
+import pandas as pd
+import pickle
+
+import tensorflow as tf
+import tensorflow.keras as keras
+from tensorflow.keras import backend as K
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.applications.vgg16 import preprocess_input
+from tensorflow.keras.callbacks import TensorBoard, EarlyStopping
+
+from keras_custom.callbacks import LangTotalLoss
+from keras_custom.models.language_model import lang_model
+from keras_custom.generators.generator_wrappers import sup_gen
+from TRAIN.utils.data_utils import load_classes, data_directory
+
+"""
+Source module for training language model
+"""
+
+def train_n_val_data_gen(subset):
+    # data generators
+    directory = data_directory(part='train')  # default is train, use val only for debug
+    wordvec_mtx = np.load('data_local/imagenet2vec/imagenet2vec_1k.npy')
+
+    #
+    #wnids, _, _ = load_classes(999, 'canidae')
+
+
+    gen, steps = sup_gen(
+                        directory=directory,
+                        classes=None,
+                        batch_size=128,
+                        seed=42,
+                        shuffle=True,
+                        subset=subset,
+                        validation_split=0.1,
+                        class_mode='categorical',  # only used for lang due to BERT indexing
+                        target_size=(224, 224),
+                        preprocessing_function=preprocess_input,
+                        horizontal_flip=True, 
+                        wordvec_mtx=wordvec_mtx)
+    return gen, steps
+
+
+def specific_callbacks(run_name):
+    """
+    Define earlystopping and tensorboard.
+    """
+    earlystopping = tf.keras.callbacks.EarlyStopping(
+                    monitor='val_loss', 
+                    min_delta=0, 
+                    patience=5, 
+                    verbose=2, 
+                    mode='min',
+                    baseline=None, 
+                    restore_best_weights=True
+                    )
+    tensorboard = tf.keras.callbacks.TensorBoard(log_dir=f'log/{run_name}')
+    return earlystopping, tensorboard
+
+
+def execute():
+    ###################################################
+    lr = 3e-5
+    lossW = 0
+    version = '16-7-20'
+    discrete_frozen = False
+    w2_depth = 2
+    run_name = f'{version}-lr={str(lr)}-lossW={lossW}'
+    ###################################################
+    # model
+    model = lang_model(w2_depth=w2_depth, discrete_frozen=discrete_frozen)
+    opt = tf.train.experimental.enable_mixed_precision_graph_rewrite(opt=Adam(lr=lr))
+    model.compile(opt,
+                  loss=['mse', 'categorical_crossentropy'],
+                  loss_weights=[1, lossW],
+                  metrics=['acc'])
+
+    # load in trained discrete weights for cases other than 1:1
+    if discrete_frozen:
+        with open(f'_trained_weights/discrete_weights-{version}-lr={lr}-lossW=1.pkl', 'rb') as f:
+            discrete_weights = pickle.load(f)
+        model.get_layer('discrete').set_weights([discrete_weights[0], discrete_weights[1]])
+        print('loaded trained discrete weights')
+    
+    # data
+    train_gen, train_steps = train_n_val_data_gen(subset='training')
+    val_gen, val_steps = train_n_val_data_gen(subset='validation')
+
+    # callbacks and fitting
+    earlystopping, tensorboard = specific_callbacks(run_name=run_name)
+    model.fit(train_gen,
+                epochs=1, 
+                verbose=1, 
+                #callbacks=[earlystopping, tensorboard],
+                validation_data=val_gen, 
+                steps_per_epoch=1,
+                validation_steps=1,
+                max_queue_size=40, workers=3, 
+                use_multiprocessing=False)
+    
+
+    ### save weights including w2 dense, semantic, and discrete --- 
+    # w2 dense
+    for i in range(w2_depth):
+        dense_ws = model.get_layer(f'w2_dense_{i}').get_weights()
+        with open(f'_trained_weights/w2_dense_{i}-{run_name}.pkl', 'wb') as f:
+            pickle.dump(dense_ws, f)
+
+    # semantic
+    semantic_ws = model.get_layer('semantic').get_weights()
+    with open(f'_trained_weights/semantic_weights-{run_name}.pkl', 'wb') as f:
+            pickle.dump(semantic_ws, f)
+
+    # save discrete too if w3 were notfrozen
+    if not discrete_frozen:
+        discrete_weights = model.get_layer('discrete').get_weights()
+        with open(f'_trained_weights/discrete_weights-{run_name}.pkl', 'wb') as f:
+                pickle.dump(discrete_weights, f)
--- keras_custom/generators/generator_wrappers.py
+++ keras_custom/generators/generator_wrappers.py
@@ -4,6 +4,7 @@ import pandas as pd
 from tensorflow.keras.applications.vgg16 import preprocess_input
 from .labels_corrector import wnids_to_network_indices, indices_rematch
 from . import language_model_generator as langGen
+from . import superGroupGenerator as supGen
 
 """
 Customer generator wrapper for Language model only.
@@ -96,6 +97,66 @@ def lang_gen(directory,
     # the initial generator
     # using Sequence
     bad_generator = langGen.SafeDirectoryIterator(
+                        directory=directory,
+                        classes=classes,
+                        batch_size=batch_size,
+                        seed=seed,
+                        shuffle=shuffle,
+                        subset=subset,
+                        validation_split=validation_split,
+                        class_mode=class_mode,
+                        target_size=target_size,
+                        preprocessing_function=preprocessing_function,
+                        horizontal_flip=horizontal_flip,
+                        wordvec_mtx=wordvec_mtx   ###
+                        )
+
+    steps = bad_generator.compute_step_size()
+
+    # label correction
+    if classes == None:
+        # when use all 1000 categories, there is no need to rematch
+        # keras-auto labelled indices to the real network indices
+        # because keras labels all categories in the order of wnids which is
+        # the same as network indices
+        # so the bad_generator is already index correct!
+        index_correct_generator = bad_generator
+    else:
+        # Sanity check: network_indices are also sorted in ascending order
+        network_indices = wnids_to_network_indices(sorted_classes)
+
+        # rematch indices and get the index_correct_generator
+        index_correct_generator = indices_rematch(bad_generator, network_indices)
+
+    good_generator = index_correct_generator
+    return good_generator, steps
+
+
+def sup_gen(directory,
+                classes,
+                batch_size,
+                seed,
+                shuffle,
+                subset,
+                validation_split,
+                class_mode,
+                target_size,
+                preprocessing_function,
+                horizontal_flip,
+                wordvec_mtx,
+                ):
+    """
+    Nothing special about this generator,
+    i.e. no branches, no pseudo input, no momentum
+    """
+    if classes == None:
+        pass
+    else:
+        sorted_classes = sorted(classes)
+
+    # the initial generator
+    # using Sequence
+    bad_generator = supGen.SafeDirectoryIterator(
                         directory=directory,
                         classes=classes,
                         batch_size=batch_size,
