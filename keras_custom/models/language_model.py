1421e54688c53f86db9d15c0edc86ee51c004453
Xiaoliang (Ken) Luo <ken@love17.pals.ucl.ac.uk>
2021-05-07 00:39:46 +0100

    rm load config

--- keras_custom/models/language_model.py
+++ keras_custom/models/language_model.py
@@ -95,15 +95,7 @@ def lang_model_contrastive(config):
      -----
           We use config to modify the setup of the model.
           When config is None, the model is the pretrained SIMCLR itself.
-     """
-
-     # TODO: this needs get out of model
-     def load_config(config_version):
-          with open(os.path.join('configs', f'{config_version}.yaml')) as f:
-               config = yaml.safe_load(f)
-          print(f'[Check] Loading [{config_version}]')
-          return config
-    
+     """    
      class Model(tf.keras.Model):
           def __init__(self, config):
                """
@@ -111,7 +103,7 @@ def lang_model_contrastive(config):
                And add the same layers as in previous version.
                """
                super(Model, self).__init__()
-               self.config = load_config(config)
+               self.config = config
 
                if self.config is not None:
                     self.saved_model = tf.saved_model.load(self.config['path'])
