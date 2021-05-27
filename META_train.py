a07122466d18d4ac9ff840deca42050d02a2437c
Xiaoliang (Ken) Luo <ken@love21.pals.ucl.ac.uk>
2021-05-27 19:07:05 +0100

    gu, train more finegrain v3 with tfrecords

--- META_train.py
+++ META_train.py
@@ -1,6 +1,6 @@
 import os
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"]= '1'
+os.environ["CUDA_VISIBLE_DEVICES"]= '3'
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 import argparse
