bf9d6bf711c25e25d69e7b56b086d6bbde85f340
Xiaoliang (Ken) Luo <ken@love21.pals.ucl.ac.uk>
2021-06-18 19:07:19 +0100

    add option to load vgg16 tfrecords

--- TRAIN/utils/data_utils.py
+++ TRAIN/utils/data_utils.py
@@ -53,7 +53,7 @@ def load_classes(num_classes, df='imagenetA'):
     return wnids.tolist(), indices, descriptions
 
 
-def data_directory(part, tfrecords=False):
+def data_directory(part, front_end, tfrecords=False):
     """
     Check which server we are on and return the corresponding 
     imagenet data directory.
@@ -69,12 +69,12 @@ def data_directory(part, tfrecords=False):
         print(f'server_num = {server_num}')
         if server_num <= 20:
             if tfrecords:
-                data_dir = f'/mnt/fast-data{server_num}/datasets/ken/simclr_reprs/{part}'
+                data_dir = f'/mnt/fast-data{server_num}/datasets/ken/{front_end}_reprs/{part}'
             else:
                 data_dir = f'/mnt/fast-data{server_num}/datasets/ILSVRC/2012/clsloc/{part}'
         else:
             if tfrecords:
-                data_dir = f'/fast-data{server_num}/datasets/ken/simclr_reprs/{part}'
+                data_dir = f'/fast-data{server_num}/datasets/ken/{front_end}_reprs/{part}'
             else:
                 data_dir = f'/fast-data{server_num}/datasets/ILSVRC/2012/clsloc/{part}'
     return data_dir
